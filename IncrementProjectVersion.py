1adade228b2441e142a858ae828c5a04f2411f0c
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2025-01-19 21:33:39 +0200

    ci: added support for incrementing all version parts.

--- IncrementProjectVersion.py
+++ IncrementProjectVersion.py
@@ -7,6 +7,7 @@ from typing import List
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
     parser.add_argument(
         dest="action",
         type=str,
@@ -20,12 +21,31 @@ if __name__ == "__main__":
         const="NONE",
     )
 
+    parser.add_argument(
+        dest="version_part",
+        type=str,
+        nargs="?",
+        choices=[
+            "NONE",
+            "MAJOR",
+            "MINOR",
+            "MICRO",
+            "NANO",
+        ],
+        default="NONE",
+        const="NONE",
+    )
+
     args = parser.parse_args()
 
     if args.action == "NONE":
         sys.exit(0)
 
     elif args.action == "INCREMENT_VERSION":
+
+        if args.version_part == "NONE":
+            sys.exit(0)
+
         current_working_directory = os.getcwd()
         version_filepath = os.path.join(
             current_working_directory, "ProjectVersion.cmake"
@@ -36,16 +56,50 @@ if __name__ == "__main__":
             version_file_lines = version_file.readlines()
 
         for line_index, line in enumerate(version_file_lines):
-            if line.startswith("set(PROJECT_VERSION_MICRO"):
-                version_micro = int(
-                    line.replace("set(PROJECT_VERSION_MICRO", "")
-                    .replace(")", "")
-                    .strip()
-                )
-                incremented_version_micro = version_micro + 1
-                version_file_lines[line_index] = (
-                    f"set(PROJECT_VERSION_MICRO {incremented_version_micro})\n"
-                )
+            if args.version_part == "MAJOR":
+                if line.startswith("set(PROJECT_VERSION_MAJOR"):
+                    version_part = int(
+                        line.replace("set(PROJECT_VERSION_MAJOR", "")
+                        .replace(")", "")
+                        .strip()
+                    )
+                    incremented_version_part = version_part + 1
+                    version_file_lines[line_index] = (
+                        f"set(PROJECT_VERSION_MAJOR {incremented_version_part})\n"
+                    )
+            elif args.version_part == "MINOR":
+                if line.startswith("set(PROJECT_VERSION_MINOR"):
+                    version_part = int(
+                        line.replace("set(PROJECT_VERSION_MINOR", "")
+                        .replace(")", "")
+                        .strip()
+                    )
+                    incremented_version_part = version_part + 1
+                    version_file_lines[line_index] = (
+                        f"set(PROJECT_VERSION_MINOR {incremented_version_part})\n"
+                    )
+            elif args.version_part == "MICRO":
+                if line.startswith("set(PROJECT_VERSION_MICRO"):
+                    version_part = int(
+                        line.replace("set(PROJECT_VERSION_MICRO", "")
+                        .replace(")", "")
+                        .strip()
+                    )
+                    incremented_version_part = version_part + 1
+                    version_file_lines[line_index] = (
+                        f"set(PROJECT_VERSION_MICRO {incremented_version_part})\n"
+                    )
+            elif args.version_part == "NANO":
+                if line.startswith("set(PROJECT_VERSION_NANO"):
+                    version_part = int(
+                        line.replace("set(PROJECT_VERSION_NANO", "")
+                        .replace(")", "")
+                        .strip()
+                    )
+                    incremented_version_part = version_part + 1
+                    version_file_lines[line_index] = (
+                        f"set(PROJECT_VERSION_NANO {incremented_version_part})\n"
+                    )
 
         with open(file=version_filepath, mode="w", encoding="utf-8") as version_file:
             version_file.writelines(version_file_lines)
