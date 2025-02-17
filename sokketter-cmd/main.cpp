0c46c5210454a9ec884d88f7f9643d249f2fabf8
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2025-02-17 22:22:50 +0200

    refactor: overriding the default help text.

--- sokketter-cmd/main.cpp
+++ sokketter-cmd/main.cpp
@@ -3,13 +3,60 @@
 #include "libsokketter.h"
 
 #include <cli11/CLI11.hpp>
+#include <sstream>
 
-class CustomFormatter : public CLI::Formatter
+class OverriddenHelpFormatter : public CLI::Formatter
 {
 public:
-    std::string make_description(const CLI::App *app) const override
+    std::string make_help(const CLI::App *app, std::string, CLI::AppFormatMode) const override
     {
-        return CLI::Formatter::make_description(app) + "\n";
+        std::stringstream help;
+
+        help << "A command-line interface for controlling attached power strips and sockets."
+             << std::endl;
+        help << std::endl;
+
+        help << "Usage: sokketter-cli SUBCOMMAND [OPTIONS]" << std::endl;
+        help << std::endl;
+
+        help << "Options:" << std::endl;
+        help << "  -h,--help\tPrints descriptive help message and exits." << std::endl;
+        help << "  -v,--version\tPrints the version of sokketter-cli." << std::endl;
+        help << std::endl;
+
+        help << "Subcommands:" << std::endl;
+        help << "  list\t\tLists all available devices." << std::endl;
+        help << std::endl;
+
+        help << "  power\t\tContains actions related to power control of the socket(s)."
+             << std::endl;
+        help << std::endl;
+
+        help << "  Subcommands:" << std::endl;
+        help << "    status\t\tStates current power state of the socket(s)." << std::endl;
+        help << "    on\t\tTurns power on the socket(s)." << std::endl;
+        help << "    off\t\tTurns power off the socket(s)." << std::endl;
+        help << "    toggle\t\tToggles power state of the socket(s)." << std::endl;
+        help << std::endl;
+
+        help << "  Options:" << std::endl;
+        help << "    -i,--device-at-index UINT\tStates which power strip to use by its index. "
+                "Excludes --device-with-serial option."
+             << std::endl;
+        help << "    -n,--device-with-serial TEXT\tStates which power strip to use by its serial "
+                "number. Excludes --device-at-index option."
+             << std::endl;
+        help << "    -s,--sockets UINT ...\tStates one or multiple socket(s) indices. Empty option "
+                "means that subcommand will apply to all available sockets."
+             << std::endl;
+        help << std::endl;
+
+        help << "Examples:" << std::endl;
+        help << "  sokketter-cli power on --sockets 1 --device-at-index 0" << std::endl;
+        help << "  sokketter-cli power status --device-with-serial 01:01:60:35:c6" << std::endl;
+        help << std::endl;
+
+        return help.str();
     }
 };
 
@@ -27,74 +74,63 @@ auto main(int argc, char *argv[]) -> int
     CLI::App application;
 
     application.name("sokketter-cli");
-    application.description(
-        "A command-line interface for controlling attached power strips and sockets");
-    application.require_subcommand();
-    application.formatter(std::make_shared<CustomFormatter>());
+    application.formatter(std::make_shared<OverriddenHelpFormatter>());
 
     /**
-     * @attention overriding default help flags to show help with subcommands.
+     * @attention overriding default help flag for easier control.
      */
     application.set_help_flag();
-    application.set_help_all_flag("--help,-h", "Prints descriptive help message and exits");
+    application.set_help_all_flag();
+    auto flag_help = application.add_flag("--help,-h");
 
     /**
      * @brief adding a version flag.
      */
-    auto flag_version = application.add_flag("--version,-v", "Prints the version of sokketter-cli");
+    auto flag_version = application.add_flag("--version,-v");
 
     /**
      * @brief adding a list subcommand.
      */
-    auto subcommand_list = application.add_subcommand("list", "Lists all available devices");
-    subcommand_list->formatter(std::make_shared<CustomFormatter>());
+    auto subcommand_list = application.add_subcommand("list");
 
     /**
      * @brief adding a power subcommand.
      */
-    auto subcommand_power = application.add_subcommand(
-        "power", "Contains actions related to power control of the socket(s)");
-    subcommand_power->formatter(std::make_shared<CustomFormatter>());
+    auto subcommand_power = application.add_subcommand("power");
 
-    auto subcommand_power_status =
-        subcommand_power->add_subcommand("status", "States current power state of the socket(s)");
-    subcommand_power_status->fallthrough();
-    auto subcommand_power_on =
-        subcommand_power->add_subcommand("on", "Turns power on the socket(s)");
-    subcommand_power_on->fallthrough();
-
-    auto subcommand_power_off =
-        subcommand_power->add_subcommand("off", "Turns power off the socket(s)");
-    subcommand_power_off->fallthrough();
-
-    auto subcommand_power_toggle =
-        subcommand_power->add_subcommand("toggle", "Toggles power state of the socket(s)");
-    subcommand_power_toggle->fallthrough();
+    auto subcommand_power_status = subcommand_power->add_subcommand("status");
+    auto subcommand_power_on = subcommand_power->add_subcommand("on");
+    auto subcommand_power_off = subcommand_power->add_subcommand("off");
+    auto subcommand_power_toggle = subcommand_power->add_subcommand("toggle");
 
     /**
      * @brief adding device and socket access options.
      */
     std::vector<size_t> socket_indices;
-    auto sockets_argument = subcommand_power->add_option(
-        "--sockets,-s", socket_indices, "States one or multiple socket(s) index");
+    auto sockets_argument = subcommand_power->add_option("--sockets,-s", socket_indices);
 
     auto device_group = subcommand_power->add_option_group("Device selection");
 
     size_t device_index = 0;
-    auto option_device_index = device_group->add_option(
-        "--device-by-index,-i", device_index, "States which power strip to use by its index");
+    auto option_device_index = device_group->add_option("--device-at-index,-i", device_index);
 
     std::string device_serial = "";
-    auto option_device_serial = device_group->add_option("--device-by-serial,-n", device_serial,
-        "States which power strip to use by its serial number");
+    auto option_device_serial = device_group->add_option("--device-with-serial,-n", device_serial);
 
     option_device_index->excludes(option_device_serial);
     option_device_serial->excludes(option_device_index);
 
     /**
-     * @brief states short hash of last Git commit
+     * @attention overwriting the default help to show the same text for all subcommands.
      */
-    application.footer("Possible things include betingalw, chiz, and res.");
+    const auto commands = {subcommand_list, subcommand_power, subcommand_power_status,
+        subcommand_power_on, subcommand_power_off, subcommand_power_toggle};
+    for (const auto &command : commands)
+    {
+        command->set_help_flag();
+        command->set_help_all_flag();
+        command->fallthrough();
+    }
 
     /** ************************************************************************
      *
@@ -108,6 +144,17 @@ auto main(int argc, char *argv[]) -> int
     catch (const CLI::ParseError &e)
     {
         return application.exit(e);
+    }
+
+    /** ************************************************************************
+     *
+     * @brief help processing section.
+     *
+     ** ***********************************************************************/
+    if (flag_help->count() > 0)
+    {
+        std::cout << application.help() << std::endl;
+        return EXIT_SUCCESS;
     }
 
     /** ************************************************************************
@@ -152,7 +199,7 @@ auto main(int argc, char *argv[]) -> int
      * @brief power processing section.
      *
      ** ***********************************************************************/
-    if (subcommand_power->parsed())
+    else if (subcommand_power->parsed())
     {
         std::unique_ptr<sokketter::power_strip> device;
 
@@ -265,7 +312,7 @@ auto main(int argc, char *argv[]) -> int
      * @brief no CLI parameters section.
      *
      ** ***********************************************************************/
-    std::cerr << "No command line parameters were specified!" << std::endl << std::endl;
+    std::cerr << "Error: list or power subcommand is required!" << std::endl << std::endl;
 
     std::cerr << application.help() << std::endl;
 
