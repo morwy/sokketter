398f67d54efb7aca049ef276f9cebbbcd890639d
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2025-03-30 22:25:51 +0300

    chores: added initial power strip UI list item.

---
+++ sokketter-ui/sources/power_strip_list_item.h
@@ -0,0 +1,30 @@
+#ifndef POWER_STRIP_LIST_ITEM_H
+#define POWER_STRIP_LIST_ITEM_H
+
+#pragma once
+
+#include <libsokketter.h>
+
+#include <QWidget>
+
+namespace Ui {
+    class power_strip_list_item;
+}
+
+class power_strip_list_item : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit power_strip_list_item(
+        const sokketter::power_strip_configuration &configuration, QWidget *parent = nullptr);
+    ~power_strip_list_item();
+
+    auto configuration() const -> const sokketter::power_strip_configuration &;
+
+private:
+    Ui::power_strip_list_item *m_ui;
+    const sokketter::power_strip_configuration m_configuration;
+};
+
+#endif // POWER_STRIP_LIST_ITEM_H
