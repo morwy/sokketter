7470958557c728c99321efcd8fc50583a9eac811
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2025-04-06 19:12:22 +0300

    chores: small UI corrections for socket_list_item.

--- sokketter-ui/sources/socket_list_item.cpp
+++ sokketter-ui/sources/socket_list_item.cpp
@@ -2,16 +2,19 @@
 #include "ui_socket_list_item.h"
 
 socket_list_item::socket_list_item(const sokketter::power_strip_configuration &power_strip,
-    const sokketter::socket_configuration &socket, QWidget *parent)
+    const sokketter::socket_configuration &socket, const size_t &socket_index, QWidget *parent)
     : QWidget(parent)
     , m_ui(new Ui::socket_list_item)
     , m_power_strip(power_strip)
     , m_socket(socket)
+    , m_socket_index(socket_index)
 {
     m_ui->setupUi(this);
 
+    const auto &index_text = "Socket " + QString::number(socket_index);
+
+    m_ui->index_label->setText(index_text);
     m_ui->name_label->setText(QString::fromStdString(socket.name));
-    m_ui->description_label->setText(QString::fromStdString(socket.id));
 }
 
 socket_list_item::~socket_list_item()
@@ -32,5 +35,6 @@ auto socket_list_item::socket_configuration() const -> const sokketter::socket_c
 
 void socket_list_item::set_state(const bool is_on) const
 {
-    m_ui->status_label->setText(is_on ? "on" : "off");
+    m_ui->status_label->setState(is_on);
+    m_ui->status_label->setToolTip(is_on ? tr("powered on") : tr("powered off"));
 }
