b3218d8adcbbc946ce94ec6404cddf488b61a551
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2025-06-28 21:44:36 +0300

    cicd: debug logging

--- Build.py
+++ Build.py
@@ -313,6 +313,21 @@ class Build:
 
         self.logger.info("Packaging completed successfully.")
 
+    def __print_file_tree(self, folder_path, indent=""):
+        try:
+            items = os.listdir(folder_path)
+        except PermissionError:
+            print(indent + "Permission Denied: " + folder_path)
+            return
+
+        for item in items:
+            item_path = os.path.join(folder_path, item)
+            if os.path.isdir(item_path):
+                print(indent + item + "/")
+                self.__print_file_tree(item_path, indent + "    ")
+            else:
+                print(indent + item)
+
     def run(self) -> None:
         """
         Run the build process.
@@ -320,6 +335,8 @@ class Build:
         self.__configure()
         self.__build()
         self.__package()
+
+        self.__print_file_tree(self.results_output_dir)
 
 
 # --------------------------------------------------------------------------------------------------
