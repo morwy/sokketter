242a9838caf3470e84880bc62c55b7123e7e4dd1
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2025-06-28 17:00:25 +0300

    fix: made socket list item to have more detailed information (#16)

--- sokketter-ui/sources/Qt/SocketListItem.cpp
+++ sokketter-ui/sources/Qt/SocketListItem.cpp
@@ -1,15 +1,15 @@
-#include "socket_list_item.h"
-#include "ui_socket_list_item.h"
+#include "SocketListItem.h"
+#include "ui_SocketListItem.h"
 
 #include <spdlog/spdlog.h>
 #include <theme_stylesheets.h>
 
 #include <QStyleHints>
 
-socket_list_item::socket_list_item(const sokketter::power_strip_configuration &power_strip,
+SocketListItem::SocketListItem(const sokketter::power_strip_configuration &power_strip,
     const sokketter::socket_configuration &socket, const size_t &socket_index, QWidget *parent)
     : QWidget(parent)
-    , m_ui(new Ui::socket_list_item)
+    , m_ui(new Ui::SocketListItem)
     , m_power_strip(power_strip)
     , m_socket(socket)
     , m_socket_index(socket_index)
@@ -19,37 +19,46 @@ socket_list_item::socket_list_item(const sokketter::power_strip_configuration &p
     m_ui->status_label->setState(QLedLabel::State::StateUnknown);
     m_ui->status_label->setToolTip(tr("unknown"));
 
-    const auto &index_text = "Socket " + QString::number(socket_index);
+    const auto &title_text =
+        "**Socket " + QString::number(socket_index) + "**: " + QString::fromStdString(socket.name);
 
-    m_ui->index_label->setText(index_text);
-    m_ui->name_label->setText(QString::fromStdString(socket.name));
+    m_ui->name_label->setText(title_text);
+
+    if (!socket.description.empty())
+    {
+        m_ui->description_label->setText(QString::fromStdString(socket.description));
+    }
+    else
+    {
+        m_ui->description_label->hide();
+    }
 
     setThemeAccordingToMode();
 }
 
-socket_list_item::~socket_list_item()
+SocketListItem::~SocketListItem()
 {
     delete m_ui;
 }
 
-auto socket_list_item::power_strip_configuration() const
+auto SocketListItem::power_strip_configuration() const
     -> const sokketter::power_strip_configuration &
 {
     return m_power_strip;
 }
 
-auto socket_list_item::socket_configuration() const -> const sokketter::socket_configuration &
+auto SocketListItem::socket_configuration() const -> const sokketter::socket_configuration &
 {
     return m_socket;
 }
 
-void socket_list_item::set_state(const bool is_on) const
+void SocketListItem::set_state(const bool is_on) const
 {
     m_ui->status_label->setState(is_on);
     m_ui->status_label->setToolTip(is_on ? tr("powered on") : tr("powered off"));
 }
 
-auto socket_list_item::event(QEvent *event) -> bool
+auto SocketListItem::event(QEvent *event) -> bool
 {
     if (event->type() == QEvent::ThemeChange || event->type() == QEvent::PaletteChange)
     {
@@ -61,7 +70,7 @@ auto socket_list_item::event(QEvent *event) -> bool
     return QWidget::event(event);
 }
 
-void socket_list_item::setThemeAccordingToMode()
+void SocketListItem::setThemeAccordingToMode()
 {
     QPixmap pixmap;
 
