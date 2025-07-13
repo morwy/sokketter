ad0cf5efa601dcdbe280ef59423030df201f24b1
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2025-07-13 17:20:15 +0300

    fix: fixed crash during initialization of libsokketter in sokketter-cli application

--- sokketter-cli/sources/main.cpp
+++ sokketter-cli/sources/main.cpp
@@ -1,6 +1,14 @@
 #include "cli_parser.h"
 
+#include "libsokketter.h"
+
 auto main(int argc, char *argv[]) -> int
 {
-    return cli_parser::parse_and_process(argc, argv);
+    sokketter::initialize();
+
+    const int return_code = cli_parser::parse_and_process(argc, argv);
+
+    sokketter::deinitialize();
+
+    return return_code;
 }
